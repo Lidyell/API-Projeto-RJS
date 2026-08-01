6de15ce99cdd6a03adc293791fae639afdb07886
joaopedro-s-eng <xjoaopedro35@gmail.com>
2026-07-31 22:24:36 -0300

    NADA

--- src/main/java/com/example/todolist/Creation/Archive/ControllerCreation.java
+++ src/main/java/com/example/todolist/Creation/Archive/ControllerCreation.java
@@ -2,6 +2,8 @@ package com.example.todolist.Creation.Archive;
 
 public class ControllerCreation {
 
-
+        public int Soma(int n){
+            return n+=10;
+        }
 
 }
--- src/main/java/com/example/todolist/TodolistApplication.java
+++ src/main/java/com/example/todolist/TodolistApplication.java
@@ -2,6 +2,7 @@ package com.example.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.todolist.Creation.Archive.ControllerCreation;
 
 @SpringBootApplication
 public class TodolistApplication {
@@ -9,6 +10,10 @@ public class TodolistApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
 
+		ControllerCreation ctrl = new ControllerCreation();
+		int no = 10;
+		int result = ctrl.Soma(no);
+		System.out.println(result);
 	}
 
 }

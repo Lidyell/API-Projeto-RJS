4891f5c610087d56d21d7bab9dbadbc6e1f63fa7
Lidyell <lidyellrocha.rs@gmail.com>
2026-08-11 12:01:04 -0300

    Ocultando credenciais

--- src/main/java/com/example/todolist/Manipulation/Archive/ArchiveController.java
+++ src/main/java/com/example/todolist/Manipulation/Archive/ArchiveController.java
@@ -1,7 +1,7 @@
 package com.example.todolist.Manipulation.Archive;
 
 public class ArchiveController {
-
-
-
+    public static void main(String[] args) {
+        System.out.println("Hello World Javas");
+    }
 }
